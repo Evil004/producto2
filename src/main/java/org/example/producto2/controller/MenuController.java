b316306982b02450c2cbd784a6afd013415d81fb
BlueeLighty00 <davidsernamateu@gmail.com>
2024-01-11 16:14:12 +0100

    Pedidos, Menus Controller

--- src/main/java/org/example/producto2/controller/MenuController.java
+++ src/main/java/org/example/producto2/controller/MenuController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class MenuController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/menus")
+    public String menus() {
+        return "menus";
     }
 
 }
