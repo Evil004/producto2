0ffa5d3296b3ef4a8013e196e718b5ea601de43f
Oscar <10717328@ieselcaminas.org>
2024-01-15 20:20:17 +0100

    home

---
+++ src/main/java/org/example/producto2/controller/ProductosController.java
@@ -0,0 +1,14 @@
+package org.example.producto2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductosController {
+
+    @GetMapping("/productos")
+    public String pedidos() {
+        return "productos";
+    }
+
+}
