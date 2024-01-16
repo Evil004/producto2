f833b25446165a41fff44b8e40fe0572246c06ce
Oscar <10717328@ieselcaminas.org>
2024-01-16 19:21:06 +0100

    register form done

--- src/main/java/org/example/producto2/controller/RegisterController.java
+++ src/main/java/org/example/producto2/controller/RegisterController.java
@@ -1,32 +1,40 @@
 package org.example.producto2.controller;
 
 import org.example.producto2.models.dao.UsuarioDAO;
+import org.example.producto2.models.dao.UsuarioDAOImpl;
 import org.example.producto2.models.entity.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
 public class RegisterController {
-
+    @Autowired
+    private
+    UsuarioDAOImpl UsuarioService;
     @GetMapping("/register")
-    public String registerForm(){
+    public String registerForm(Model model){
+
+        model.addAttribute("usuario", new Usuario());
+
         return "registerForm";
     }
 
-    @PostMapping("/registerPost")
+    @PostMapping(value = "/register", consumes = "application/x-www-form-urlencoded\n")
     @ResponseStatus(HttpStatus.CREATED)
-    public String registerSubmit(@RequestBody Usuario usuario) {
+    public ResponseEntity<String> registerSubmit(@ModelAttribute Usuario usuario) {
 
-        return "redirect:/";
+        UsuarioService.save(usuario);
+
+        return ResponseEntity.ok("POST request successful");
+
     }
 
 }
--- src/main/java/org/example/producto2/security/SecurityConfig.java
+++ src/main/java/org/example/producto2/security/SecurityConfig.java
@@ -4,11 +4,16 @@ import org.example.producto2.models.dao.UsuarioDAO;
 import org.example.producto2.models.entity.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -34,9 +39,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(configurer ->
-                        configurer.requestMatchers("/*").permitAll()
+                        configurer.requestMatchers(antMatcher(HttpMethod.POST, "/*")).permitAll()
+                                .requestMatchers("/*").permitAll()
                                 .anyRequest().authenticated()
-                )
+                ).csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form ->
                         form
                                 .loginPage("/login")
